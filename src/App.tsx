6f21b9ea2c310be82fa0696ef282b8d14e61076b
ThomasTheDebugEngine <thomaskilinc09@gmail.com>
2021-12-20 07:53:48 +0200

    commit work so far

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StyledEngineProvider } from "@mui/styled-engine";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./pages/HomePageComponent/HomePage-Component";
+import ProfilePage from "./pages/ProfilePageComponent/ProfilePage-Component";
+import BrowsePage from "./pages/BrowsePageComponent/BrowsePage-Component";
+import LoginPage from "./pages/LoginPageComponent/LoginPage-Component";
+import EditorPage from "./pages/EditorPageComponent/EditorPage-Component";
+import RegisterPage from "./pages/RegisterPageComponent/RegisterPage-Component";
+import ExpandPage from "./pages/ExpandPageComponent/ExpandPage-Component";
+import SecureRoute from "./components/SecureRoute/SecureRoute-Component";
+
+import './App.scss';
+
+export class App extends React.Component<any, any> {
+    componentDidMount() {
+        // this.props.purge() //for testing with persistance (and logout)
+    }
+
+    render() {
+        return (
+            <StyledEngineProvider injectFirst>
+                <div className="App">
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/browse" element={<BrowsePage />} />
+
+
+                        <Route path="/profile"
+                            element={
+                                <SecureRoute>
+                                    <ProfilePage />
+                                </SecureRoute>
+                            } />
+
+                        <Route path="/editor"
+                            element={
+                                <SecureRoute>
+                                    <EditorPage />
+                                </SecureRoute>
+                            } />
+
+                        <Route path="/projects/:projectId/view"
+                            element={
+                                <SecureRoute>
+                                    <ExpandPage />
+                                </SecureRoute>
+                            } />
+                    </Routes>
+                </div>
+            </StyledEngineProvider>
+        );
+    }
 }
-
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+
+import './index.scss';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
